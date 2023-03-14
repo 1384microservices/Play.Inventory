d4fe21f2fb5c4bbcce301033f1c59fb995bf2c83
Andrei Tudor <andrei.tudor@hotmail.com>
2023-03-14 15:38:59 +0200

    Add CORS support

--- src/Play.Inventory.Service/Startup.cs
+++ src/Play.Inventory.Service/Startup.cs
@@ -52,6 +52,10 @@ public class Startup
             app.UseDeveloperExceptionPage();
             app.UseSwagger();
             app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Play.Inventory.Service v1"));
+            app.UseCors(cfg =>
+            {
+                cfg.WithOrigins(Configuration["AllowedOrigin"]).AllowAnyHeader().AllowAnyMethod();
+            });
         }
 
         app.UseHttpsRedirection();
