31a4a2dfe4322299e4ec2160d20933d032a6d85d
Andrei Tudor <andrei.tudor@hotmail.com>
2023-03-21 00:56:42 +0200

    Add RBAC, users and roles seed

--- src/Play.Inventory.Service/Startup.cs
+++ src/Play.Inventory.Service/Startup.cs
@@ -1,17 +1,13 @@
-using System;
-using System.Net.Http;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using Play.Common.Identity;
 using Play.Common.MassTransit;
 using Play.Common.MongoDB;
-using Play.Inventory.Service.Clients;
 using Play.Inventory.Service.Entities;
-using Polly;
-using Polly.Timeout;
 
 namespace Play.Inventory.Service;
 
@@ -30,9 +26,10 @@ public class Startup
         services
         .AddMongo()
         .AddMongoRepository<InventoryItem>("inventoryitems")
-        .AddMongoRepository<CatalogItem>("catalogitems");
-
-        services.AddMassTransitWithRabbitMQ();
+        .AddMongoRepository<CatalogItem>("catalogitems")
+        .AddMassTransitWithRabbitMQ()
+        .AddJwtBearerAuthentication()
+        ;
 
         // RetryPolicies(services);
 
@@ -59,35 +56,32 @@ public class Startup
         }
 
         app.UseHttpsRedirection();
-
         app.UseRouting();
-
+        app.UseAuthentication();
         app.UseAuthorization();
-
         app.UseEndpoints(endpoints =>
         {
             endpoints.MapControllers();
         });
     }
 
-    private static void RetryPolicies(IServiceCollection services)
-    {
-        Random jitter = new Random();
-
-        services
-        .AddHttpClient<CatalogClient>(c =>
-        {
-            c.BaseAddress = new Uri("https://localhost:5001");
-        })
-        .AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().WaitAndRetryAsync(
-                2,
-                retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)) + TimeSpan.FromMilliseconds(jitter.Next(0, 999)),
-                onRetry: (outcome, timespan, retryAttempt) =>
-                {
-                    Console.WriteLine($"Delaying for {timespan.TotalSeconds} seconds, then making retry {retryAttempt}");
-                })
-        )
-        .AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().CircuitBreakerAsync(3, TimeSpan.FromSeconds(15)))
-        .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(3));
-    }
+    // private static void RetryPolicies(IServiceCollection services)
+    // {
+    //     Random jitter = new Random();
+    //     services
+    //     .AddHttpClient<CatalogClient>(c =>
+    //     {
+    //         c.BaseAddress = new Uri("https://localhost:5001");
+    //     })
+    //     .AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().WaitAndRetryAsync(
+    //             2,
+    //             retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)) + TimeSpan.FromMilliseconds(jitter.Next(0, 999)),
+    //             onRetry: (outcome, timespan, retryAttempt) =>
+    //             {
+    //                 Console.WriteLine($"Delaying for {timespan.TotalSeconds} seconds, then making retry {retryAttempt}");
+    //             })
+    //     )
+    //     .AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().CircuitBreakerAsync(3, TimeSpan.FromSeconds(15)))
+    //     .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(3));
+    // }
 }
